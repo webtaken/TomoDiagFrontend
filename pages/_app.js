50666717c391509be3c2f73293a1ee4d8b45058e
saul <luckly083@gmail.com>
2022-11-27 08:36:26 -0500

    google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,32 @@
-import Navbar from '../components/Navbar'
-import { AuthContextProvider } from '../stores/authContext'
-
-import 'antd/dist/antd.css';
-import '../styles/globals.css';
+import Navbar from "../components/Navbar";
+import { AuthContextProvider } from "../stores/authContext";
+import Script from "next/script";
+import "antd/dist/antd.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <AuthContextProvider>
-    <Component {...pageProps} />
-  </AuthContextProvider>
+  return (
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        async
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-BEYT663BHX"
+      ></Script>
+      <Script strategy="lazyOnload">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BEYT663BHX');    
+    `}
+      </Script>
+      <AuthContextProvider>
+        <Component {...pageProps} />
+      </AuthContextProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
